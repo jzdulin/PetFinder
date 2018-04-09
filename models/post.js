73f0e40ed2428f5c175f09d713d154c3d22f746e
Joshua Dulin <jzdulin@wifi_stu-10-38-81-130.uncc.edu>
2018-04-09 12:58:00 -0400

    added api routes for all cats and specific cat

---
+++ models/post.js
@@ -0,0 +1,26 @@
+//TO BE USED LATER FOR COMMENTS SECTION
+
+// module.exports = function(sequelize, DataTypes) {
+//     var Post = sequelize.define("Post", {
+//       name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//           len: [1]
+//         }
+//       },
+//       post: {
+//         type: DataTypes.TEXT,
+//         allowNull: false,
+//         validate: {
+//           len: [1]
+//         }
+//       },
+//       category: {
+//         type: DataTypes.STRING,
+//         defaultValue: "General"
+//       }
+//     });
+//     return Post;
+//   };
+  
--- routes/api-routes.js
+++
@@ -1,8 +0,0 @@
-// var db = require("../models");
-
-// module.exports = function(app) {
-
-//     app.ger("/api/dogs/", function(req, res) {
-
-//     })
-// }
