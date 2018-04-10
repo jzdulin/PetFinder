158cd5e8d364ff7ef25871b354342e54d2af895f
Joshua Dulin <jzdulin@wifi_stu-10-38-75-176.uncc.edu>
2018-04-10 13:32:36 -0400

    added comment functionality

--- routes/comment-api-routes.js
+++ routes/comment-api-routes.js
@@ -1,46 +1,58 @@
-// var db = require("../models");
+var db = require("../models");
 
-// module.exports = function(app) {
+module.exports = function(app) {
 
-    //DISPLAY ALL THE COMMENTS
-    // app.get("/api/comments/", function(req, res) {
-    //     db.Post.findAll({})
-    //         .then(function(dbPost) {
-    //             res.json(dbPost);
-    //     });
-    // });
+    // DISPLAY ALL THE COMMENTS
+    app.get("/api/comments/", function(req, res) {
+        db.Post.findAll({})
+            .then(function(dbPost) {
+                res.json(dbPost);
+        });
+    });
 
-    //POST A NEW COMMENT
-    // app.post("/api/comments", function(req, res) {
-    //     db.Post.create({
-    //         name: req.body.name,
-    //         comment: req.body.comment,
-    //     }).then(function(dbPost) {
-    //         res.json(dbPost);
-    //     });
-    // });
+    // DISPLAY POSTS OF A CATEGORY
+    app.get("/api/posts/breed/:breed", function(req, res) {
+        db.Post.findAll({
+            where: {
+                breed: req.params.breed,
+            }
+        })
+        .then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
 
-    //DELETE A COMMENT
-    // app.delete("/api/comments/:id", function(req, res) {
-    //     db.Post.destroy({
-    //         where: {
-    //             id: req.params.id
-    //         }
-    //     }).then(function(dbPost) {
-    //         res.json(dbPost);
-    //     });
-    // });
+    // POST A NEW COMMENT
+    app.post("/api/comments", function(req, res) {
+        db.Post.create({
+            name: req.body.name,
+            comment: req.body.comment,
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
 
-    //UPDATE A COMMENT
-    // app.put("/api/comments", function(req, res) {
-    //     db.Post.update(
-    //         req.body,
-    //     {
-    //         where: {
-    //             id:req.body.id
-    //         }
-    //     }).then(function(dbPost) {
-    //         res.json(dbPost);
-    //     });
-    // });
-// }
+    // DELETE A COMMENT
+    app.delete("/api/comments/:id", function(req, res) {
+        db.Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+    // UPDATE A COMMENT
+    app.put("/api/comments", function(req, res) {
+        db.Post.update(
+            req.body,
+        {
+            where: {
+                id:req.body.id
+            }
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
+}
