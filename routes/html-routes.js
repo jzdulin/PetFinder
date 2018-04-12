553265afbbff9d7453755e4ff22727d8732b1a80
Joshua Dulin <jzdulin@wifi_stu-10-38-74-68.uncc.edu>
2018-04-12 13:54:26 -0400

    changed comment section

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,26 +15,5 @@ module.exports = function(app) {
   app.get("/cat", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/cat.html"))
   });
-
-  app.get("/api/comments/:catId", function(req, res) {
-    // var breed = req.params.breed;
-    //make a db call here to search for the cat breed, then pass the results 
-    // into a handlebars object for rendering
-
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    console.log(Object.keys(db.Cat));
-    db.Post.findAll({
-        where: {
-            CatId: req.params.catId,
-            // include: [db.Cat]
-        }
-    }).then(function(dbPost) {
-        res.json(dbPost)
-        // res.render("index", {posts: dbPost});
-    });
-
-    });
  
 };
